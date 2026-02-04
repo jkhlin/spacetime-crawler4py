1d993ba66e9d5a7ca8d28327423ccb04788134b6
“Jason <jkhlin@uci.edu>
2026-02-04 11:36:26 -0800

    get rid of mpeg because its a large file

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ ALLOWED_HOST_NAMES = re.compile(r"^(?:.*\.)?(?:ics|cs|informatics|stat)\.uci\.ed
 BAD_EXTENSIONS = re.compile(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|wav|avi|mov|mpe?g|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
