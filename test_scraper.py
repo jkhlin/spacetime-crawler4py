6d78a9d8c61091d974ea9f4f2e557232bc3cb547
Aidan Huerta <aidanmh@uci.edu>
2026-02-03 23:01:44 -0800

    Updated is_valid logic + test file

--- test_scraper.py
+++ test_scraper.py
@@ -1,9 +1,35 @@
 # TODO: delete when done
-
+import re
+from urllib.parse import urlparse
 from scraper import is_valid
 
-print(is_valid("http://www.ics.uci.edu/"))
-print(is_valid("http://google.com/"))
-print(is_valid("http://www.ics.uci.edu/foo/bar"))
-print(is_valid("http://www.ics.uci.edu/a/b/a/b/a/b"))
-print(is_valid("http://www.ics.uci.edu/calendar"))
+###  Test RegEx ###
+
+example_url = "http://www.ics.uci.edu:443"
+example_url_path = "http://www.ics.uci.edu/foo/bar"
+
+print(f"RegEx Hostname Test:\n")
+parsed = urlparse(example_url)
+print(f"URL: {example_url}\nHost Name: {parsed.hostname}\n")
+
+print(f"RegEx Path Test:\n")
+parsed = urlparse(example_url_path)
+print(f"URL: {example_url}\nPath: {parsed.path}\n")
+
+### is_valid test ###
+
+url_list = ["http://www.ics.uci.edu/", "http://google.com/", "http://www.ics.uci.edu/foo/bar",
+            "http://www.ics.uci.edu/a/b/a/b/a/b", "http://www.ics.uci.edu/calendar"]
+
+print(f"is_valid Test:\n")
+
+for urls in url_list:
+    print(f"{urls} -> {is_valid(urls)}")
+
+print("\nEND OF TESTS")
+
+# print(is_valid("http://www.ics.uci.edu/"))
+# print(is_valid("http://google.com/"))
+# print(is_valid("http://www.ics.uci.edu/foo/bar"))
+# print(is_valid("http://www.ics.uci.edu/a/b/a/b/a/b"))
+# print(is_valid("http://www.ics.uci.edu/calendar"))
