71a4ee7927655806ae1f95c54e5ff375d49b535a
“Jason <jkhlin@uci.edu>
2026-02-05 15:58:36 -0800

    revert some bad_extensions that took a while to crawl since they may have been good links

--- scraper.py
+++ scraper.py
@@ -16,10 +16,10 @@ NON_CONTENT_ELEMENTS = [
 ]
 
 BAD_EXTENSIONS = re.compile(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            r".*\.(css|js|bmp|gif|jpeg|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpe?g|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|ppsx|doc|docx|xls|xlsx|names"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
